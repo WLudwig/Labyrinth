109ea82e6758db8735c2581ff7090b8b5094b2be
Will Ludwig <williampludwig@gmail.com>
2019-11-17 20:32:54 -0700

    Basic Implementation of our website

---
+++ CS4540Final/CS4540Final/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CS4540Final.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

75c32bd51d1d270731552ccc6a59f49cd8b8c13d
Will Ludwig <williampludwig@gmail.com>
2019-12-04 10:52:42 -0700

    Updated menus so that they appear for appropriate roles.  Implemented an admin pay for high scores that allows them to delete high scores

--- CS4540Final/CS4540Final/Controllers/HomeController.cs
+++ CS4540Final/CS4540Final/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using CS4540Final.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace CS4540Final.Controllers
 {
@@ -14,6 +15,7 @@ namespace CS4540Final.Controllers
     {
 
         private readonly HighScoreDB context;
+
 
         public HomeController(HighScoreDB _context)
         {
@@ -24,15 +26,15 @@ namespace CS4540Final.Controllers
         {
             return View();
         }
-        /*
-            public IActionResult void HighScore()
-            {
-                return View(await context.HighScore.ToListAsync());
-            }
-            */
 
         [Authorize(Roles = "Player")]
         public async Task<IActionResult> HighScore()
+        {
+            return View(await context.HighScore.ToListAsync());
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> AdminHighScore()
         {
             return View(await context.HighScore.ToListAsync());
         }
@@ -53,6 +55,23 @@ namespace CS4540Final.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var highscore = await context.HighScore.FirstOrDefaultAsync(m => m.HighScoreID == id);
+            if(highscore == null)
+            {
+                return NotFound();
+            }
+            context.HighScore.Remove(highscore);
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(AdminHighScore));
         }
     }
 }
